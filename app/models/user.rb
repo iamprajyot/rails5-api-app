0b724accc6fd4d3809ed79f7fc4e10302e24c3b3
Prajyot Rajigare <prajyot.rajigare27@gmail.com>
2017-09-13 14:45:10 +0530

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # class User < ActiveRecord::Base
+  devise :database_authenticatable, :recoverable, :validatable
+
+  after_create :update_access_token!
+  
+  validates :username, presence: true
+  validates :email, presence: true
+
+  private
+
+  def update_access_token!
+    self.access_token = generate_access_token
+    save
+  end
+
+  def generate_access_token
+    loop do
+      token = "#{self.id}:#{Devise.friendly_token}"
+      break token unless User.where(access_token: token).first
+    end
+  end
+end
---
+++ db/migrate/20170913082918_add_access_token_and_username_to_user.rb
@@ -0,0 +1,6 @@
+class AddAccessTokenAndUsernameToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :access_token, :string
+    add_column :users, :username, :string
+  end
+end
